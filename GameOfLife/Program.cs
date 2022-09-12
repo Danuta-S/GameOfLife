e3f879d45e51659fa5a28319a259b35226679093
danuta.supole <danuta.supole@gmail.com>
2022-09-12 10:54:42 +0300

    Created the app based on the defined algorithm with a simple navigation menu for the user to select size of field.

--- GameOfLife/Program.cs
+++ GameOfLife/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using GameOfLifeLibrary;
+
+namespace GameOfLife
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserOutput.ShowMenu();
+            // Use initializeRandomBoard for a larger, random board.
+            Logic.InitializeRandomBoard();
+            Logic.InitializeConsole();
+
+            // Run the game until the S key is pressed.
+            while (!Console.KeyAvailable || Console.ReadKey(true).Key != ConsoleKey.S)
+            {
+                Logic.DrawBoard();
+                Logic.UpdateBoard();
+
+                // Wait for a bit between updates.
+                Thread.Sleep(GameOfLifeUI.delay);
+            }
+        }  
+    }
+}
---
+++ GameOfLifeLibrary/UserOutput.cs
@@ -0,0 +1,20 @@
+﻿using GameOfLife;
+using System;
+namespace GameOfLifeLibrary
+{
+    public class UserOutput
+    {
+        /// <summary>
+        /// Shows navigation menu and possibility for the user to select size of field.
+        /// </summary>
+        public static void ShowMenu()
+        {
+            Console.WriteLine("Welcome to the Game Of Life!" + Environment.NewLine);
+            Console.WriteLine("Please enter the size of field.");
+            Console.Write("Width: ");
+            GameOfLifeUI.width= int.Parse(Console.ReadLine());
+            Console.Write("Height: ");
+            GameOfLifeUI.height = int.Parse(Console.ReadLine());
+        }
+    }
+}
